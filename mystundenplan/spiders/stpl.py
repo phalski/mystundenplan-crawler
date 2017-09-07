65fff117b1c3c7acd535d61b81a1933e9fc83426
Philipp <phalski@users.noreply.github.com>
2017-09-07 23:19:19 +0200

    Implementing crawler

--- mystundenplan/spiders/stpl.py
+++ mystundenplan/spiders/stpl.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Selector
 from scrapy.exceptions import CloseSpider
+from scrapy.loader.processors import SelectJmes, Compose, MapCompose
+
+import json
+import jmespath
 from mystundenplan.items import SessionItem
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from w3lib.url import url_query_cleaner
 
 
 class StplSpider(scrapy.Spider):
+    VALID_SESSION_LEN = 40
+
     name = 'stpl'
-    allowed_domains = ['wwww3.primuss.de']
+    allowed_domains = ['www3.primuss.de']
 
     def __init__(self, login_url=None, tenant=None, username=None, password=None, *args, **kwargs):
         super(StplSpider, self).__init__(*args, **kwargs)
@@ -35,24 +42,101 @@ class StplSpider(scrapy.Spider):
             "Prepared login request for %s:%s@%s at %s" % (self.username, self.password, self.tenant, self.login_url))
         return [request]
 
+    def cbsem_request(self):
+        if not self.session:
+            raise CloseSpider("Can't create request without active session")
+
+        return self.newRequest(self.session['url'],
+                               {
+                                   'FH': self.session['fh']
+                               },
+                               {
+                                   'mode': 'cbsem',
+                                   'Session': self.session['session'],
+                                   'User': self.session['user']
+                               },
+                               self.scrape_cbsem)
+
+    def index_requests(self, sems):
+        if not self.session:
+            raise CloseSpider("Can't create request without active session")
+
+        requests = []
+        for sem in sems:
+            requests.append(self.newRequest(self.session['url'],
+                                            {
+                                                'FH': self.session['fh']
+                                            },
+                                            {
+                                                'User': self.session['user'],
+                                                'Session': self.session['session'],
+                                                'sem': sem
+                                            },
+                                            self.scrape_index))
+
+        return requests
+
+    def cbgrid_raum_requests(self, sem, raums):
+        if not self.session:
+            raise CloseSpider("Can't create request without active session")
+
+        requests = []
+        for raum in raums:
+            requests.append(self.newRequest(self.session['url'],
+                                            {
+                                                'FH': self.session['fh'],
+                                                'sem': sem,
+                                                'raum': raum
+                                            },
+                                            {
+                                                'mode': raum,
+                                                'Session': self.session['session'],
+                                                'User': self.session['user']
+                                            },
+                                            self.scrape_index))
+
+        return requests
+
+    def calendar_raum_requests(self, sem, raums):
+        if not self.session:
+            raise CloseSpider("Can't create request without active session")
+
+        requests = []
+        for raum in raums:
+            requests.append(self.newRequest(self.session['url'],
+                                            {
+                                                'FH': self.session['fh'],
+                                                'sem': sem,
+                                                'method': 'list'
+                                            },
+                                            {
+                                                'Session': self.session['session'],
+                                                'User': self.session['user'],
+                                                'mode': 'calendar',
+                                                'raum': raum
+                                            },
+                                            self.scrape_index))
+
+        return requests
+
     def scrape_session(self, response):
-        parsed_url = urlparse(response.url)
-        parsed_query = parse_qs(parsed_url.query)
+        url_parsed = urlparse(response.url)
+        query_parsed = parse_qs(url_parsed.query)
 
         try:
             session = SessionItem(
                 url=url_query_cleaner(response.url),
-                fh=parsed_query['FH'][0],
-                lang=parsed_query['Lang'][0],
-                user=parsed_query['User'][0],
-                session=parsed_query['Session'][0],
+                fh=query_parsed['FH'][0],
+                lang=query_parsed['Lang'][0],
+                user=query_parsed['User'][0],
+                session=query_parsed['Session'][0],
             )
 
             if not (session['url'] and not session['url'] == self.login_url and
-                    session['fh'] and
-                    session['lang'] and
-                    session['user'] and
-                    session['session'] and 40 == len(session['session'])
+                        session['fh'] and
+                        session['lang'] and
+                        session['user'] and
+                        session['session'] and StplSpider.VALID_SESSION_LEN == len(session['session'])
                     ):
                 raise CloseSpider("No valid session found. Please check your credentials!")
 
@@ -62,9 +146,23 @@ class StplSpider(scrapy.Spider):
         except KeyError:
             raise CloseSpider("No valid session found. Please check your credentials!")
 
-        self.parse(response)
+        return self.parse(response)
 
     def parse(self, response):
-        if not self.session:
-            raise CloseSpider("No active session present")
-        pass
+        return [self.cbsem_request()]
+
+    def scrape_cbsem(self, response):
+        return self.index_requests(jmespath.search('[].id', json.loads(response.text)))
+
+    def scrape_index(self, response):
+        selector = Selector(text=response.body)
+        raums = selector.css("#cbraum > option:not(:first-child)").css("::attr(value)").extract()
+        requests = []
+        requests.extend(self.cbgrid_raum_requests('33', raums))
+        requests.extend(self.calendar_raum_requests('33', raums))
+        return requests
+
+    def newRequest(self, url, querydata, formdata, callback):
+        url_parsed = urlparse(url)
+        url_parsed = url_parsed._replace(query=urlencode(querydata))
+        return scrapy.FormRequest(urlunparse(url_parsed), formdata=formdata, callback=callback)
