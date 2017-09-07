60f51aae989ae332a04943648c76cdf3f101fefa
Philipp <phalski@users.noreply.github.com>
2017-09-07 21:08:29 +0200

    Scraping session info

--- mystundenplan/items.py
+++ mystundenplan/items.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class MystundenplanItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class SessionItem(scrapy.Item):
+    url = scrapy.Field()
+    fh = scrapy.Field()
+    lang = scrapy.Field()
+    user = scrapy.Field()
+    session = scrapy.Field()
--- mystundenplan/spiders/stpl.py
+++ mystundenplan/spiders/stpl.py
@@ -1,11 +1,70 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.exceptions import CloseSpider
+from mystundenplan.items import SessionItem
+from urllib.parse import urlparse, parse_qs
+from w3lib.url import url_query_cleaner
 
 
 class StplSpider(scrapy.Spider):
     name = 'stpl'
     allowed_domains = ['wwww3.primuss.de']
-    start_urls = ['http://wwww3.primuss.de/']
+
+    def __init__(self, login_url=None, tenant=None, username=None, password=None, *args, **kwargs):
+        super(StplSpider, self).__init__(*args, **kwargs)
+
+        if not (login_url and tenant and username and password):
+            self.logger.critical('Spider opened with missing arguments')
+            raise ValueError('Spider opened with missing arguments')
+
+        self.login_url = login_url
+        self.tenant = tenant
+        self.username = username
+        self.password = password
+
+        self.session = None
+
+    def start_requests(self):
+        request = scrapy.FormRequest(self.login_url, formdata={
+            'user': self.username,
+            'pwd': self.password,
+            'mode': 'login',
+            'FH': self.tenant
+        }, callback=self.scrape_session)
+        self.logger.debug(
+            "Prepared login request for %s:%s@%s at %s" % (self.username, self.password, self.tenant, self.login_url))
+        return [request]
+
+    def scrape_session(self, response):
+        parsed_url = urlparse(response.url)
+        parsed_query = parse_qs(parsed_url.query)
+
+        try:
+            session = SessionItem(
+                url=url_query_cleaner(response.url),
+                fh=parsed_query['FH'][0],
+                lang=parsed_query['Lang'][0],
+                user=parsed_query['User'][0],
+                session=parsed_query['Session'][0],
+            )
+
+            if not (session['url'] and not session['url'] == self.login_url and
+                    session['fh'] and
+                    session['lang'] and
+                    session['user'] and
+                    session['session'] and 40 == len(session['session'])
+                    ):
+                raise CloseSpider("No valid session found. Please check your credentials!")
+
+            self.session = session
+            self.logger.info("Created session %s" % dict(self.session))
+
+        except KeyError:
+            raise CloseSpider("No valid session found. Please check your credentials!")
+
+        self.parse(response)
 
     def parse(self, response):
+        if not self.session:
+            raise CloseSpider("No active session present")
         pass
